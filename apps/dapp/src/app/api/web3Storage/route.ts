16167809aa4133f4b4f708515d6984ffd4b76e73
MichaelGrinstead <michael.a.grinstead@gmail.com>
2024-02-26 13:54:13 +1000

    WIP

--- apps/dapp/src/app/api/web3Storage/route.ts
+++ apps/dapp/src/app/api/web3Storage/route.ts
@@ -1,20 +1,30 @@
-import * as Client from '@web3-storage/w3up-client';
-
-const spaceDID = process.env
-  .NEXT_PUBLIC_WEB3_STORAGE_SPACE_DID as `did:key:${string}`;
+import { create } from '@web3-storage/w3up-client';
+// const spaceDID = process.env
+//   .NEXT_PUBLIC_WEB3_STORAGE_SPACE_DID as `did:key:${string}`;
 
 export async function POST(request: Request): Promise<Response> {
-  const client = await Client.create();
-  const account = await client.login('mike@modadao.io');
-  // const space = await client.createSpace('drop-protocol');
-  // await account.provision(space.did());
+  try {
+    console.log('creating client');
+    const client = await create();
+    const space = await client.createSpace('my-awesome-space');
+    console.log('DID', space.did());
+    console.log('logging in');
+    await client.login('nic@modadao.io');
+    // console.log('setting space');
+    // await client.setCurrentSpace(
+    //   'did:key:z6Mktdqp71MacqtubCvR1YcrGVSwcXgtac1K7dbQXd7yVgEJ',
+    // );
 
-  const formData = await request.formData();
-  const file = formData.get('file') as Blob;
+    const formData = await request.formData();
+    const file = formData.get('file') as Blob;
+    const CID = await client.uploadFile(file);
+    console.log(CID);
+  } catch (e) {
+    console.log('\n\n\n\n\n\n web3 storage error', (e as any).message);
+  }
+  // await account.provision(spaceDID);
 
-  const CID = await client.uploadFile(file);
+  // console.log(CID);
 
-  console.log(CID);
-
-  return new Response(JSON.stringify({ CID }), { status: 200 });
+  return new Response(JSON.stringify({}), { status: 200 });
 }
