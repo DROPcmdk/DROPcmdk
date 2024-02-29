78f3f5795f8c1f111c2b5873d7f3cb853d4f471b
MichaelGrinstead <michael.a.grinstead@gmail.com>
2024-02-29 11:03:28 +1000

    WIP

--- apps/dapp/src/app/api/web3Storage/route.ts
+++ apps/dapp/src/app/api/web3Storage/route.ts
@@ -1,19 +1,11 @@
 import { create } from '@web3-storage/w3up-client';
-// const spaceDID = process.env
-//   .NEXT_PUBLIC_WEB3_STORAGE_SPACE_DID as `did:key:${string}`;
+const spaceDID = process.env
+  .NEXT_PUBLIC_WEB3_STORAGE_SPACE_DID as `did:key:${string}`;
 
 export async function POST(request: Request): Promise<Response> {
   try {
-    console.log('creating client');
     const client = await create();
-    const space = await client.createSpace('my-awesome-space');
-    console.log('DID', space.did());
-    console.log('logging in');
-    await client.login('nic@modadao.io');
-    // console.log('setting space');
-    // await client.setCurrentSpace(
-    //   'did:key:z6Mktdqp71MacqtubCvR1YcrGVSwcXgtac1K7dbQXd7yVgEJ',
-    // );
+    await client.setCurrentSpace(spaceDID);
 
     const formData = await request.formData();
     const file = formData.get('file') as Blob;
